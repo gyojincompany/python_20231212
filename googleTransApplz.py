ba735353f149d3d10db8e80fc39f655c1f42b238
gyojincompany <luckyred@gmail.com>
2023-12-15 19:04:52 +0900

    구글번역프로그램 완성

--- googleTransApplz.py
+++ googleTransApplz.py
@@ -18,6 +18,26 @@ class MyGoogleTrans(QMainWindow, form_class):
         self.setWindowIcon(QIcon("icon/google.png"))
         self.statusBar().showMessage("Google Trans Applz v1.0 Copyrightⓒ 2023")
 
+        self.transButton.clicked.connect(self.transFunction)
+        self.resetButton.clicked.connect(self.resetFunction)
+
+    def transFunction(self):
+        trans = googletrans.Translator()  # 구글 번역객체 생성
+        trans_kor = self.inputEdit.text()  # 사용자가 입력한 텍스트(한글)를 가져옴
+
+        result_eng = trans.translate(trans_kor, dest='en')  # 영어로 번역
+        result_jap = trans.translate(trans_kor, dest='ja')  # 일어로 번역
+        result_chn = trans.translate(trans_kor, dest='zh-cn')  # 중국어로 번역
+
+        self.engOutput.append(result_eng.text)
+        self.japOutput.append(result_jap.text)
+        self.chnOutput.append(result_chn.text)
+
+    def resetFunction(self):
+        self.inputEdit.clear()
+
+
+
 app = QApplication(sys.argv)
 myProgram = MyGoogleTrans()
 myProgram.show()
